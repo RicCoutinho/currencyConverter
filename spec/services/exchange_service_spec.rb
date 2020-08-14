c856a79c2393d9b78287db99e04ce31c0ae32e22
ricardo <dinhofonte@hotmail.com>
2020-08-13 23:18:34 -0300

    created exchange_service test

---
+++ spec/services/exchange_service_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+require 'json'
+require './app/services/exchange_service'
+
+describe ExchangeService do 
+  let(:source_currency) { 'USD' }
+  let(:target_currecy) { 'BRL' }
+  let(:exchange_value) { 3.45 }
+  let(:api_return) do
+    {
+      currency: [
+        currency: "#{source_currency}/#{target_currecy}",
+        value: exchange_value,
+        date: Time.now,
+        type: 'Original'
+      ]
+    }
+  end
+
+  before do
+    allow(RestClient).to receive(:get) { OpenStruct.new(body: api_return.json) }
+  end
+
+  describe '.call' do
+    context 'when amount is given'
+      it 'returns amount * exchange_value' do
+        amount = rand(0..9999)
+        service_exchange = ExchangeService.new('USD', 'BRL', amount).call
+        expect_exchange = amount * exchange_value
+
+        expect(service_exchange).to eq(expect_exchange)  
+      end
+  end
+end
